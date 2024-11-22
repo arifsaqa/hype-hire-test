4fe5e65f2606e161a7043996872b7cb7fc78d450
Syamsul Arifin <arifsaqa@gmail.com>
2024-11-22 22:34:13 +0700

    init: project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      padding: '2rem',
+      center: true,
+    },
     extend: {
+      backgroundImage: {
+        hero: "linear-gradient(151.17deg, rgba(38, 194, 185, 0.9) 8.69%, rgba(40, 139, 231, 0.9) 126.06%), url('/images/background.png')",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      fontFamily: {
+        pop: ['var(--font-poppin)'],
       },
     },
   },
