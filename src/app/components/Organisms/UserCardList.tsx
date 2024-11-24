d58996d112a055a760f01e5472301afcc23f3b09
Syamsul Arifin <arifsaqa@gmail.com>
2024-11-24 23:09:24 +0700

    finishing cardlist

--- src/app/components/Organisms/UserCardList.tsx
+++ src/app/components/Organisms/UserCardList.tsx
@@ -2,14 +2,16 @@
 import { User } from '@/types';
 import React from 'react';
 import UserCard from '../Molecules/UserCard';
+import { Chevron } from '../Atoms/Icons';
 
 const UserCardList = ({ userList }: { userList: Array<User> }) => {
-
   return (
-    <div className='relative'>
+    <div className="relative w-full min-h-[26rem]">
+      <Chevron className="top-1/2 left-5 absolute cursor-pointer" size={25} direction='left'/>
       {userList.map((user) => (
-          <UserCard {...user} key={user.id}/>
+        <UserCard {...user} key={user.id} />
       ))}
+      <Chevron className="top-1/2 right-5 absolute cursor-pointer" size={25} direction='right'/>
     </div>
   );
 };
